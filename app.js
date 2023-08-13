3c99e31aa183de1700abdccd986aee7754a3802e
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-08-13 22:21:08 +0530

    fix: add bootstrap and redesign index page

--- app.js
+++ app.js
@@ -1,53 +1,15 @@
 const express = require("express");
 const http = require("http");
-const{ SerialPort} = require("serialport");
-const{ ReadlineParser} = require("@serialport/parser-readline");
-const { Socket } = require("socket.io");
-
 
 const app = express();
-// https.listen(1500);
-const server = http.createServer(app);
+
 app.use(express.static("public"));
-const parser = new ReadlineParser({delimiter: '\n'});
+const server = http.createServer(app);
 
-
-// socket.io
-var io = require("socket.io").Server;
-var socketIO = new io(server);
-
-var datas;
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
 server.listen(3000, function () {
   console.log("Server is running on port 3000.");
-})
-
-// var serialport = new SerialPort({
-//     path: "/dev/ttyACM0",
-//     baudRate: 9600
-//  });
- 
-//  serialport.open((err)=>{
-//      if(err){
-//          console.log("error opening the port"+err.message);
-//      }
-//  });
- 
-//  serialport.pipe(parser);
-//  parser.on("data",(data)=>{
-//   datas = data.toString();
-//   // console.log(datas);
-//   socketIO.emit("message",datas);
-
-//  });
-
-
-
-
-
-
-
-
+});
