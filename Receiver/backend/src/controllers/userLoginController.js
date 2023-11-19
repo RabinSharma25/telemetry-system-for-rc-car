8aa281d694cbc369a423408d5850a871ed5b1188
RabinSharma25 <curzosharmas22545@gmail.com>
2023-11-19 22:48:07 +0530

    Made the login and register api endpoints functional

--- Receiver/backend/src/controllers/userLoginController.js
+++ Receiver/backend/src/controllers/userLoginController.js
@@ -3,38 +3,56 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const { LoginReq, LoginRes } = require('../dto/userLoginDTO');
 const UserLoginService = require('../services/userLoginService');
 
 router.use(bodyParser.json());
+router.use(cookieParser());
 router.post('/login', async (req, res) => {
     try {
 
-
         let { email, password } = req.body;
-        let firstName, lastName, id, name, message, success;
+        let message, success,jwt;
         const loginReq = new LoginReq({ email, password })
-        const user = await UserLoginService.findUserByEmail(email);
 
-        console.log("Input json ");
-        console.log(loginReq);
 
-        id = user.id
-        firstName = user.firstName
-        lastName = user.lastName
-        name = firstName + " " + lastName
-        let eml = user.email
-        let passwrd = user.password
-        success = true;
-        message = "Successfully Logged in"
+        const authResult = await UserLoginService.authenticateUser(loginReq);
 
-        const isMatch =  UserLoginService.comparePasswords(password,passwrd);
-        if(isMatch){
-        const loginRes = new LoginRes({ name, message, success });
-        console.log(loginRes);
-        res.status(201).json(loginRes);
+
+        if (authResult) {
+            const { token } = authResult;
+            res.cookie('jwt', token, { httpOnly: true });
+            success = true;
+            message = "Suuccessfully logged in";
+            jwt = token
+            const loginRes = new LoginRes({message,success,jwt})
+            res.json(loginRes);
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid email or password' });
         }
-    } catch (error) {
+    }
+
+    // console.log("Input json ");
+    // console.log(loginReq);
+
+    // id = user.id
+    // firstName = user.firstName
+    // lastName = user.lastName
+    // name = firstName + " " + lastName
+    // let eml = user.email
+    // let passwrd = user.password
+    // success = true;
+    // message = "Successfully Logged in"
+
+    // const isMatch =  UserLoginService.comparePasswords(password,passwrd);
+    // if(isMatch){
+    // const loginRes = new LoginRes({ name, message, success });
+    // console.log(loginRes);
+    // res.status(201).json(loginRes);
+    // }
+    catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ error: 'Error creating user' });
     }
--- Receiver/backend/src/services/userLoginService.js
+++ Receiver/backend/src/services/userLoginService.js
@@ -1,28 +1,64 @@
 
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'your_secret_key'; // Replace with a secure secret key
+
+
 UserLoginRepository = require('../repositories/userLoginRepository')
 async function findUserByEmail(email) {
     return UserLoginRepository.findUserByEmail(email);
 }
 
-async function hashPassword(password) {
-    try {
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
-      return hashedPassword;
-    } catch (error) {
-      console.error('Error hashing password:', error.message);
-      return false;
-    }
-  }
-  
-  // Function to check if a password matches a hashed password
-  async function comparePasswords(plainPassword, hashedPassword) {
-    try {
-      const match = await bcrypt.compare(plainPassword, hashedPassword);
-      return match;
-    } catch (error) {
-      console.error('Error comparing passwords:', error.message);
-      return false;
-    }
-  }
 
-module.exports = {findUserByEmail, hashPassword ,comparePasswords}
+
+// Function to check if a password matches a hashed password
+async function comparePasswords(plainPassword, hashedPassword) {
+  try {
+    const match = await bcrypt.compare(plainPassword, hashedPassword);
+    return match;
+  } catch (error) {
+    console.error('Error comparing passwords:', error.message);
+    return false;
+  }
+}
+
+// Function to generate a JWT token
+function generateToken(userId) {
+  const payload = { userId };
+  const options = { expiresIn: '1h' }; // Set the expiration time as needed
+  const token = jwt.sign(payload, secretKey, options);
+  return token;
+}
+
+// Function to verify a JWT token
+function verifyToken(token) {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded;
+  } catch (error) {
+    console.error('Error verifying token:', error.message);
+    return null;
+  }
+}
+
+// Function to authenticate a user and generate a JWT token
+async function authenticateUser(data) {
+  try {
+    const user = await findUserByEmail(data.email);
+  
+
+    if (user && (await comparePasswords(data.password, user.password))) {
+      const token = generateToken(user.id);
+      return { user: user.toJSON(), token };
+    } else {
+      throw new Error('Invalid email or password');
+    }
+  } catch (error) {
+    throw new Error('Authentication failed: ' + error.message);
+  }
+}
+
+module.exports = {comparePasswords, generateToken, verifyToken, authenticateUser };
