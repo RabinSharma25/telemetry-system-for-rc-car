9ae8643a93980b7479a93b6aa07a7a9fdd4722be
RabinSharma25 <curzosharmas22545@gmail.com>
2023-12-24 15:03:21 +0530

    Made changes in the charge.js to map the incoming voltage to percentage

--- Receiver/frontend/Charge/charge.js
+++ Receiver/frontend/Charge/charge.js
@@ -4,7 +4,7 @@ const chargeElement = document.getElementById("charge");
 const chargingTimeRef = document.getElementById("charging-time");
 let dataArray;
 let value;
-
+let val = 100;
 
 function  updateLevelInfo(val) {
   let batteryLevel = val + "%";
@@ -21,8 +21,24 @@ socket.addEventListener("message", (event) => {
   event.preventDefault(); // Prevent the default behavior
   dataArray = event.data.split(',');
   console.log(dataArray);
-  value = parseFloat(dataArray[5]);
-  // console.log(value);
-  updateLevelInfo(value);
+  value = parseFloat(dataArray[6]);
+  let mappedValue = mapRange((value-6), 0, 2.4, 0, 100);
+  if(mappedValue<val){
+    val = mappedValue;
+  }
+  updateLevelInfo(val);
 });
+
+function mapRange(value, fromMin, fromMax, toMin, toMax) {
+  // Ensure the value is within the from range
+  value = Math.min(Math.max(value, fromMin), fromMax);
+
+  // Calculate the percentage of the value within the from range
+  const percentage = (value - fromMin) / (fromMax - fromMin);
+
+  // Map the percentage to the to range
+  const mappedValue = Math.round(percentage * (toMax - toMin) + toMin);
+
+  return mappedValue;
+}
 
