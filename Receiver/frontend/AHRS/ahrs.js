ad5885831916008cd204bff657f630515903c6c5
RabinSharma25 <cruzosharmas22545@gmail.com>
2023-12-30 23:05:46 +0530

    Changed the visual from a simple box to a car like object in AHRS

--- Receiver/frontend/AHRS/ahrs.js
+++ Receiver/frontend/AHRS/ahrs.js
@@ -1,98 +1,123 @@
+
+import * as THREE from 'three';
+
 function degrees_to_radians(degrees) {
-	var pi = Math.PI;
-	return degrees * (pi / 180);
+    var pi = Math.PI;
+    return degrees * (pi / 180);
 }
-
-
-
-// /***************socket.io********************/
-let roll;
-let pitch;
-let yaw;
-
-// let data;
-let dataArray;
-
 
 const socket = new WebSocket("ws://localhost:3000");
 
-socket.addEventListener("message", (event) => {
-  event.preventDefault(); // Prevent the default behavior
-  dataArray = event.data.split(',');
-  roll = degrees_to_radians(parseFloat(dataArray[0]));
-  pitch = degrees_to_radians(parseFloat(dataArray[1]));
-  yaw = degrees_to_radians(parseFloat(dataArray[2]));
-  console.log("Imu data",roll,pitch,yaw)
+let roll, pitch, yaw;
+let dataArray;
 
+socket.addEventListener("message", (event) => {
+    event.preventDefault();
+    dataArray = event.data.split(',');
+    roll = degrees_to_radians(parseFloat(dataArray[0]));
+    pitch = degrees_to_radians(parseFloat(dataArray[1]));
+    yaw = degrees_to_radians(parseFloat(dataArray[2]));
+    console.log("Imu data", roll, pitch, yaw);
 });
-import * as THREE from 'three';
-var scene3d = document.getElementById("scene3d");
-var w = scene3d.offsetWidth;
-var h = scene3d.offsetHeight;
+
+const scene3d = document.getElementById("scene3d");
+const w = scene3d.offsetWidth;
+const h = scene3d.offsetHeight;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h, false);
 scene3d.appendChild(renderer.domElement);
-var euler = new THREE.Euler(pitch, yaw, roll, "XYZ");
-var meshA = new THREE.Mesh(
-	new THREE.BoxGeometry(1,1,1), // length, breadth, height
-	new THREE.MeshNormalMaterial());
 
-// cloning this mesh
-var box1 = meshA.clone();
-box1.rotation.copy(euler);
-box1.position.set(0, 0, 0);
-scene.add(box1);
+const euler = new THREE.Euler(pitch, yaw, roll);
+const carGeometry = new THREE.BoxGeometry(3, 0.5, 1.7);
+const wheelGeometry = new THREE.SphereGeometry(0.4, 20, 20);
 
-// axes helper 
+// Different colors for each side of the car
+const carMaterials = [
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Front
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // Back
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Top
+    new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Bottom
+    new THREE.MeshBasicMaterial({ color: 0xff00ff }), // Right
+    new THREE.MeshBasicMaterial({ color: 0x00ffff })  // Left
+];
 
-const axesHelper = new THREE.AxesHelper( 10 );
-// camera.lookAt(axesHelper.position);
+// Different colors for each wheel
+const wheelMaterials = [
+    new THREE.MeshBasicMaterial({ color: 0x333333 }), // Front left wheel
+    new THREE.MeshBasicMaterial({ color: 0x666666 }), // Front right wheel
+    new THREE.MeshBasicMaterial({ color: 0x999999 }), // Rear left wheel
+    new THREE.MeshBasicMaterial({ color: 0xcccccc })  // Rear right wheel
+];
 
-scene.add( axesHelper );
-axesHelper.setColors(0x0000ff,0xff0000,0x013220 );// x,y,z 
+// Create a multi-material object for the car
+const carMesh = new THREE.Mesh(carGeometry, carMaterials);
+
+// Create separate meshes for each wheel
+const frontLeftWheel = new THREE.Mesh(wheelGeometry, wheelMaterials[0]);
+const frontRightWheel = new THREE.Mesh(wheelGeometry, wheelMaterials[1]);
+const rearLeftWheel = new THREE.Mesh(wheelGeometry, wheelMaterials[2]);
+const rearRightWheel = new THREE.Mesh(wheelGeometry, wheelMaterials[3]);
+
+// Position the wheels relative to the car
+frontLeftWheel.position.set(-1, 0, 1);   // Adjusted position
+frontRightWheel.position.set(1, 0, 1);    // Adjusted position
+rearLeftWheel.position.set(-1, 0, -1);    // Adjusted position
+rearRightWheel.position.set(1, 0, -1); 
+
+// Create a group to hold the car and wheels
+const carGroup = new THREE.Group();
+carGroup.add(carMesh);
+carGroup.add(frontLeftWheel);
+carGroup.add(frontRightWheel);
+carGroup.add(rearLeftWheel);
+carGroup.add(rearRightWheel);
+
+scene.add(carGroup);
+
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+axesHelper.setColors(0x0000ff, 0xff0000, 0x013220);
 
 function animate() {
-	requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-	euler = new THREE.Euler(pitch, yaw, roll);
-	// euler = new THREE.Euler(roll,pitch,yaw);
-	box1.rotation.copy(euler);
-	renderer.render(scene, camera);
+    euler.set(pitch, (yaw-180), roll);
+    carGroup.rotation.copy(euler);
 
+    renderer.render(scene, camera);
 }
 
-// Create a FontLoader to load the font for text geometry
-
-
-
 function toggleFullScreen() {
-	if (!document.fullscreenElement) {
-	  scene3d.requestFullscreen().catch((err) => {
-		console.error(`Error attempting to enable full-screen mode: ${err.message}`);
-	  });
-	} else {
-	  document.exitFullscreen();
-	}
-  }
-  
-  // Handle double-tap event
-  let lastTapTime = 0;
-  
-  scene3d.addEventListener('click', (event) => {
-	const currentTime = new Date().getTime();
-	const timeSinceLastTap = currentTime - lastTapTime;
-  
-	if (timeSinceLastTap < 300) {
-	  toggleFullScreen();
-	}
-  
-	lastTapTime = currentTime;
-  });
-animate();
+    if (!document.fullscreenElement) {
+        scene3d.requestFullscreen().catch((err) => {
+            console.error(`Error attempting to enable full-screen mode: ${err.message}`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+}
 
-//Reference 
-//https://dustinpfister.github.io/2021/04/28/threejs-euler/
+let lastTapTime = 0;
+
+scene3d.addEventListener('click', (event) => {
+    const currentTime = new Date().getTime();
+    const timeSinceLastTap = currentTime - lastTapTime;
+
+    if (timeSinceLastTap < 300) {
+        toggleFullScreen();
+    }
+
+    lastTapTime = currentTime;
+});
+
+// ... (previous code)
+
+
+
+animate();
