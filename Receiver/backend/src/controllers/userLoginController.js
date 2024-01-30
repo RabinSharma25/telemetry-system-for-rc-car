339bd5195beee89de849982e4fe537b1f4da40d3
RabinSharma25 <72149623+RabinSharma25@users.noreply.github.com>
2024-01-30 14:44:04 +0530

    Worked on the api call from gui

--- Receiver/backend/src/controllers/userLoginController.js
+++ Receiver/backend/src/controllers/userLoginController.js
@@ -1,27 +1,26 @@
-// src/controllers/userController.js
+// src/controllers/userLoginController.js
 
 const express = require('express');
-const router = express.Router();
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const router = express.Router();
+// const cookieParser = require('cookie-parser');
 
-const { LoginReq, LoginRes } = require('../dto/userLoginDTO');
 const UserLoginService = require('../services/userLoginService');
 const logger = require("../utils/logger");
 router.use(bodyParser.json());
-router.use(cookieParser());
+// router.use(cookieParser());
 
 // logger.info("User login controller");
 async function LoginUser(req, res) {
     logger.info("User login controller.")
-
+    console.log(req.body);
     const [result, err] = await UserLoginService.LoginUser(req.body);
 
     if (err != null) {
         res.status(500).json(result)
         return 0;
     }
-    res.status(201).json(result);
+    res.status(200).json(result);
     return 0;
 
 }
--- Receiver/backend/src/routes/api.js
+++ Receiver/backend/src/routes/api.js
@@ -4,10 +4,11 @@ const express = require('express');
 const userRegisterController = require('../controllers/userRegisterController');
 const userLoginController = require('../controllers/userLoginController')
 const logger = require("../utils/logger");
-const {signUpValidation} = require("../validators/validator")
+const {signUpValidation} = require("../validators/validator");
+const bodyParser = require('body-parser');
 
 const router = express.Router();
-
+router.use(bodyParser.json());
 router.use('/register', signUpValidation,userRegisterController.RegisterUser);
 router.use('/login',userLoginController.LoginUser);
 logger.info("Routes loaded successfully");
