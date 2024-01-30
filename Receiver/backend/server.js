96dc2a1776cb6bb0a12ddc927d68f327e3c90ad6
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-01-30 15:10:18 +0530

    Removed dto

--- Receiver/backend/server.js
+++ Receiver/backend/server.js
@@ -7,6 +7,7 @@ const { SerialPort } = require("serialport");
 const logger = require("./src/utils/logger");
 const { ReadlineParser } = require("@serialport/parser-readline");
 const fs = require("fs");
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
@@ -28,7 +29,7 @@ app.use((req, res, next) => {
 // app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
-// app.use(cors());
+app.use(cors());
 app.use('/api', apiRoutes); // to call use http://localhost:3000/api/users/users
 server.listen(3000, function () {
   logger.info("Server is running on port 3000.");
